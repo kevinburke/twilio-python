a3577683f2ad79ccf998f3be1bb9f3e4c4f2b2ce
Guillaume BINET <gbin@gootz.net>
2012-10-24 23:47:39 +0200

    The actual port
    Had to embed completely jwt as its python 3 compatibility is not real

--- twilio/rest/__init__.py
+++ twilio/rest/__init__.py
@@ -18,8 +18,8 @@ from twilio.rest.resources import PhoneNumbers
 from twilio.rest.resources import Conferences
 from twilio.rest.resources import Sandboxes
 from twilio.rest.resources import Usage
-from urllib import urlencode
-from urlparse import urljoin
+from twilio.compat import urlencode
+from twilio.compat import urljoin
 
 
 def find_credentials():
